b914104c2a3e2973e9b58a87b6bc09d474c58e4f
juliaheisler <heisle12@msu.edu>
2019-09-09 14:45:02 -0400

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Julia Heisler on 9/9/19.
+//  Copyright © 2019 Julia Heisler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

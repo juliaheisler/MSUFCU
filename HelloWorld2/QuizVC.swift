5e1181718155570abe03845f1189d5daabc44e58
juliaheisler <heisle12@msu.edu>
2019-09-17 20:43:30 -0400

    quiz (Implementing ViewController)

---
+++ HelloWorld2/QuizVC.swift
@@ -0,0 +1,30 @@
+//
+//  QuizVC.swift
+//  HelloWorld2
+//
+//  Created by Julia Heisler on 9/17/19.
+//  Copyright © 2019 Julia Heisler. All rights reserved.
+//
+
+import UIKit
+
+class QuizVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

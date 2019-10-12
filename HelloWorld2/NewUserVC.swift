951729ef1c8826d0736f98479a92924714d83daa
Julia Heisler <heisle12@msu.edu>
2019-10-12 19:30:37 -0400

    account page & random code for php

--- HelloWorld2/NewUserVC.swift
+++ HelloWorld2/NewUserVC.swift
@@ -13,6 +13,72 @@ class NewUserVC: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var passwordConf: UITextField!
+    
+     let URL_SAVE_TEAM = "http://35.9.22.109/get_trans.php"
+    
+    @IBAction func buttonContinue(_ sender: Any)
+    {
+        
+        //created NSURL
+        let requestURL = NSURL(string: URL_SAVE_TEAM)
+        
+        //creating NSMutableURLRequest
+        let request = NSMutableURLRequest(url: requestURL! as URL)
+        
+        //setting the method to post
+        request.httpMethod = "POST"
+        
+        //getting values from text fields
+        let user = email.text
+        let pw = password.text
+        
+        //creating the post parameter by concatenating the keys and values from text field
+        let postParameters = "email="+user!+"&password="+pw!;
+        
+        //adding the parameters to request body
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        
+        //creating a task to send the post request
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if error != nil{
+                print("error")
+                return;
+            }
+            
+            //parsing the response
+            do {
+                //converting resonse to NSDictionary
+                let myJSON =  try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                
+                //parsing the json
+                if let parseJSON = myJSON {
+                    
+                    //creating a string
+                    var msg : String!
+                    
+                    //getting the json response
+                    msg = parseJSON["message"] as! String?
+                    
+                    //printing the response
+                    print(msg)
+                    
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        //executing the task
+        task.resume()
+        
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

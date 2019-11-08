5ac3547988cb2f32dab62c3422bde64e9359ceca
juliaheisler <heisle12@msu.edu>
2019-11-08 14:36:11 -0500

    test if i can push

---
+++ testPush.swift
@@ -0,0 +1,23 @@
+//
+//  testPush.swift
+//  
+//
+//  Created by Julia Heisler on 11/8/19.
+//
+
+import UIKit
+
+class testPush: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

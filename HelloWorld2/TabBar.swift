0c58db4e3e6cbd1b2c69078082d697c4d9ec06fd
Julia Heisler <heisle12@msu.edu>
2019-11-12 15:24:17 -0500

    tab bar notification dot

---
+++ HelloWorld2/TabBar.swift
@@ -0,0 +1,30 @@
+//
+//  TabBar.swift
+//  HopesAndDreams
+//
+//  Created by Team MSUFCU on 11/12/19.
+//  Copyright © 2019 Julia Heisler. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

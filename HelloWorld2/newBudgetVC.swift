46ef1c474289b33c034dd107048192f9a081d31d
Julia Heisler <heisle12@msu.edu>
2019-11-12 19:57:26 -0500

    add budget page and dash implementation

---
+++ HelloWorld2/newBudgetVC.swift
@@ -0,0 +1,37 @@
+//
+//  newBudgetVC.swift
+//  HopesAndDreams
+//
+//  Created by Team MSUFCU on 11/12/19.
+//  Copyright © 2019 Julia Heisler. All rights reserved.
+//
+
+import UIKit
+
+class newBudgetVC: UIViewController {
+    @IBOutlet weak var category: UITextField!
+    
+    @IBOutlet weak var limit: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onAddBudget(_ sender: Any) {
+        
+        //send the budget to the API
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

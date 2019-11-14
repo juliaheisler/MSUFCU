a498c84152633dfe88e3f7fc5befad5d42a2af3c
juliaheisler <heisle12@msu.edu>
2019-11-14 15:40:46 -0500

    transfer funds

---
+++ transferGoalVC.swift
@@ -0,0 +1,30 @@
+//
+//  transferGoalVC.swift
+//  HopesAndDreams
+//
+//  Created by Julia Heisler on 11/14/19.
+//  Copyright © 2019 Julia Heisler. All rights reserved.
+//
+
+import UIKit
+
+class transferGoalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

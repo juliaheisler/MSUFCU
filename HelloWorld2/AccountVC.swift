841acd3b2de6cdfc932c8c1ffdb27002e0884e8e
Julia Heisler <heisle12@msu.edu>
2019-11-17 11:35:15 -0500

    UI changes on Dash

---
+++ HelloWorld2/AccountVC.swift
@@ -0,0 +1,31 @@
+//
+//  AccountVC.swift
+//  HopesAndDreams
+//
+//  Created by Team MSUFCU on 11/17/19.
+//  Copyright © 2019 Julia Heisler. All rights reserved.
+//
+
+import UIKit
+
+class AccountVC: UIViewController {
+
+    @IBOutlet weak var name: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
